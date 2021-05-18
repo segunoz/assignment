80d4c1d9de2bed817b3ea4e425dec514965ff180
segunoz <segunoz1@gmail.com>
2021-05-18 21:09:32 +0100

    submission

---
+++ app/src/main/java/com/segun/assignment/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.segun.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Flowable.just("sweet","sour","salty","tangy")
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.computation())
+            .subscribe(
+                {value -> println("Recieved: $value")},
+                { error -> println("Error: $error")},
+                { println("Completed")}
+            ).dispose()
+    }
+}
